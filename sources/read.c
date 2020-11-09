cc9b52ebbbdaf47b58adb6d82e13ea839faea700
Rebecca Czarfunee <rczarfun@il-b2.msk.21-school.ru>
2020-11-09 21:47:04 +0300

    new reader + pos found

--- sources/read.c
+++ sources/read.c
@@ -6,7 +6,7 @@
 /*   By: rczarfun <rczarfun@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/01 14:12:40 by rczarfun          #+#    #+#             */
-/*   Updated: 2020/11/01 14:12:51 by rczarfun         ###   ########.fr       */
+/*   Updated: 2020/11/09 21:06:57 by rczarfun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,40 +24,11 @@ static void		read_pos(int fd, t_env *e)
 	line_split = ft_strsplit(line, ' ');
 	while (line_split[++i] != '\0')
 		;
-	if (i != 4)
+	if (i != 2)
 		error_map();
-	e->map_width = ft_atoi(line_split[0]);
-	e->map_height = ft_atoi(line_split[1]);
-	e->player.pos.x = ft_atoi(line_split[2]) + 0.5;
-	e->player.pos.y = ft_atoi(line_split[3]) + 0.5;
-	if (e->map_width < 0 || e->map_height < 0 || e->player.pos.x < 0 ||
-			e->player.pos.y < 0 || e->player.pos.x >= e->map_width ||
-			e->player.pos.y >= e->map_width)
-		error_map();
-}
 
-static void		read_line(char *line, int y, int **map, t_env *e)
-{
-	int		x;
-	char	**line_split;
-
-	x = -1;
-	if (y >= e->map_height)
-		error_map();
-	line_split = ft_strsplit(line, ' ');
-	map[y] = (int *)malloc(sizeof(int *) * e->width);
-	while (line_split[++x] != '\0')
-	{
-		if (!(line_split[x][0] >= '0' && line_split[x][0] <= '9'
-					&& ft_atoi(line_split[x]) >= 0 && x < e->map_width))
-			error_map();
-		map[y][x] = ft_atoi(line_split[x]);
-		if ((x == 0 || x == e->map_width - 1 || y == 0 || y == e->map_height -
-					1) && map[y][x] == 0)
-			error_map();
-	}
-	if (x != e->map_width)
-		error_map();
+	e->player.pos.x = ft_atoi(line_split[0]) + 0.5;
+	e->player.pos.y = ft_atoi(line_split[1]) + 0.5;
 }
 
 static int		read_file(int fd, t_env *e)
@@ -65,12 +36,18 @@ static int		read_file(int fd, t_env *e)
 	char	*line;
 	int		y;
 	int		**map;
+	int		i;
 
 	y = -1;
-	read_pos(fd, e);
-	map = (int **)malloc(sizeof(int **) * e->map_height);
-	while (get_next_line(fd, &line) == 1)
-		read_line(line, ++y, map, e);
+	if  (!(map = (int **)malloc(sizeof(int **) * e->map_height)))
+			return (0);
+	i = 0;
+	get_next_line(fd, &line);
+	while (get_next_line(fd, &line))
+	{
+		map[i++] = ft_num_split(line);
+		ft_strdel(&line);
+	}
 	if (map[(int)e->player.pos.x][(int)e->player.pos.y] != 0)
 		error_map();
 	e->map = map;
@@ -80,6 +57,8 @@ static int		read_file(int fd, t_env *e)
 int				open_file(t_env *e, char *f)
 {
 	int		fd;
+	char	*line;
+	int		width_curr;
 
 	fd = open(f, O_DIRECTORY); //Зачем это?
 	if (fd >= 0)
@@ -87,5 +66,28 @@ int				open_file(t_env *e, char *f)
 	fd = open(f, O_RDONLY);
 	if (fd < 0)
 		return (0);
+	read_pos(fd, e);
+	e->map_width = -1;
+	e->map_height = 0;
+
+	while (get_next_line(fd, &line))
+	{
+		e->map_height++;
+		if (e->map_width < 0)
+			e->map_width = ft_num_ammount(line);
+		width_curr = ft_num_ammount(line);
+		ft_strdel(&line);
+		if (((e->map_width) != width_curr) || (!(width_curr > 0)))
+			error_map();
+	}
+	close(fd);
+		if (e->map_width < 0 || e->map_height < 0 || e->player.pos.x < 0 ||
+			e->player.pos.y < 0 || e->player.pos.x >= e->map_width ||
+			e->player.pos.y >= e->map_width)
+						error_map();
+
+	fd = open(f, O_RDONLY);
 	return (read_file(fd, e));
 }
+
+
