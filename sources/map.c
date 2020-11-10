c506c33221239e49e426397473c21abb5d2889fa
hlavelle <GjGeUfQxBr14>
2020-11-10 17:47:31 +0300

    упрощенная мини-карта готова. можно сделать игрока плавнее и добавить направление

--- sources/map.c
+++ sources/map.c
@@ -24,47 +24,21 @@ void	draw_player(t_env *e)
 {
 	int 	x;
 	int		y;
-//	int 	result;
-//	double 	dif;
 
 	x = (int)e->player.pos.x * 5 + 5;
-//	dif = e->player.pos.x - result;
-//	if (dif == 0)
-//		x = 5 + (e->player.pos.x * 5);
-//	else
-//	{
-//		if (dif > 0.5 || dif == 0.5)
-//			x = 10 + (e->player.pos.x * 5);
-//		else
-//			x = e->player.pos.x * 5;
-//	}
 	y = (int)e->player.pos.y * 5 + 5;
-//	dif = e->player.pos.y - result;
-//	if (dif == 0)
-//		y = 5 + (e->player.pos.y * 5);
-//	else
-//	{
-//		if (dif > 0.5 || dif == 0.5)
-//			y = 10 + (e->player.pos.y * 5);
-//		else
-//			y = e->player.pos.y * 5;
-//	}
-	draw_square(x, y, e, 0xFFFFFF);
+	draw_square(y, x, e, 0xFFFFFF);
 }
 
-void	draw_map(t_env *e) //TODO разбить функцию
+void	draw_map(t_env *e)
 {
 	int x;
-	int x1;
 	int y;
-	int y1;
 	int i;
 	int j;
 
 	x = 5;
-	x1 = 5;
 	y = 5;
-	y1 = 5;
 	i = 0;
 	j = 0;
 	while(i < e->map_height)
@@ -72,19 +46,13 @@ void	draw_map(t_env *e) //TODO разбить функцию
 		while (j < e->map_width)
 		{
 			if (e->map[i][j] == 1)
-			{
 				draw_square(x, y, e, 0xFFFF0F);
-			}
 			j++;
-			x1 += 5;
-			x = x1;
-			y = y1;
+			x += 5;
 		}
 		x = 5;
-		x1 = 5;
 		j = 0;
 		i++;
 		y += 5;
-		y1 = y;
 	}
 }
