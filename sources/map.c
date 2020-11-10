492763a0e34bcc235eb57e67f4b3bd87712b17e5
hlavelle <GjGeUfQxBr14>
2020-11-10 20:12:05 +0300

    пыталась сделать направление на мини-карте для игрока

--- sources/map.c
+++ sources/map.c
@@ -1,5 +1,32 @@
 #include "../includes/wolf.h"
 
+// TODO доделать направление игрока на карте
+//void 	draw_dir(int x, int y, t_env *e, int color)
+//{
+//	int i;
+//	int j;
+//
+//	i = 0;
+//	j = 0;
+//	if(e->player.dir.x < 0 && e->player.dir.y <= 0)
+//	{
+//		x++;
+//		y--;
+//		while(j++ < 10)
+//		{
+//			while(i++ < 4)
+//			{
+//				mlx_pixel_put(e->mlx.mlx, e->mlx.win, x++, y,
+//							  color);
+//			}
+//			i = 0;
+//			y--;
+//			x -= 4;
+//		}
+//
+//	}
+//}
+
 void 	draw_square(int x, int y, t_env *e, int color)
 {
 	int y1;
@@ -28,6 +55,7 @@ void	draw_player(t_env *e)
 	x = (int)e->player.pos.x * 5 + 5;
 	y = (int)e->player.pos.y * 5 + 5;
 	draw_square(y, x, e, 0xFFFFFF);
+	draw_dir(y, x, e, 0xFF0000);
 }
 
 void	draw_map(t_env *e)
